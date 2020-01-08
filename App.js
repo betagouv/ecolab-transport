6684ce170c1cf6fe30d7f33507a4f8c68cec1d70
Mael <mael.thomas@data.gouv.fr>
2020-01-08 16:07:15 +0100

    Un peu de style

--- App.js
+++ App.js
@@ -8,6 +8,7 @@ export default () => {
 	return (
 		<div
 			css={`
+				margin-top: 1rem;
 				display: flex;
 				flex-direction: column;
 				justify-content: space-around;
@@ -18,19 +19,35 @@ export default () => {
 				<div
 					css={`
 						display: flex;
-						justify-content: space-around;
+						justify-content: center;
 						align-items: center;
-						img {
-							width: 5rem;
-						}
 					`}
 				>
-					<img src={logoAdeme} />
-					<img src={logoEcolab} />
+					<img css="width: 4rem" src={logoAdeme} />
+					<img css="width: 10rem" src={logoEcolab} />
 				</div>
-				<h1>Mes déplacements sont-ils écolo ?</h1>
+				<h1>Mon déplacement est-il écolo ?</h1>
 			</header>
-			<section>
+			<section
+				css={`
+					input {
+						border-radius: 0.3rem;
+						border: 2px solid #7b9fc4;
+						width: 4rem;
+						margin: 0 0.3rem 0 1rem;
+						padding: 0 0.3rem;
+						text-align: right;
+					}
+					input[type='number']::-webkit-inner-spin-button,
+					input[type='number']::-webkit-outer-spin-button {
+						-webkit-appearance: none;
+					}
+
+					input[type='number'] {
+						-moz-appearance: textfield;
+					}
+				`}
+			>
 				<label>
 					Quelle distance ?
 					<input
@@ -40,12 +57,27 @@ export default () => {
 						value={distance}
 						onChange={({ target: { value } }) => setDistance(value)}
 					/>
+					km
 				</label>
 			</section>
 			<section>
-				{modes['urbains'].map(mode => (
-					<li key={mode}>{mode}</li>
-				))}
+				<h2>Votre empreinte sur le climat</h2>
+				<ul>
+					{modes['urbains'].map(mode => (
+						<li key={mode} css="margin: .6rem 0; list-style-type: none">
+							{mode}
+							<div
+								css={`
+									background: purple;
+									height: 1rem;
+									margin-top: 0.2rem;
+									border-radius: 0.4rem;
+									width: ${Math.random() * 100}%;
+								`}
+							></div>
+						</li>
+					))}
+				</ul>
 			</section>
 		</div>
 	)
