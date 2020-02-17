1f0f6760ccf88e3255453071e91a58278bc921d7
Mael <mael.thomas@data.gouv.fr>
2020-02-17 16:43:17 +0100

    :bug: Example d'iframe avec l'adresse de prod, c'est mieux

--- Integration.js
+++ Integration.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react'
 
-const src = 'http://localhost:8080/iframe.js'
+const src = 'https://ecolab-transport.netlify.com/iframe.js'
 export default ({ setRouter }) => {
 	useEffect(() => {
 		const script = document.createElement('script')
